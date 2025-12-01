ab9423327071a0de997f4ea6b25986657db5b75c
Divakar <mydigitalspace21@gmail.com>
2025-12-01 22:57:10 +0530

    added ci feature

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,11 +1,31 @@
 import { render, screen } from "@testing-library/react";
-import App from "../App";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "../auth/AuthContext";
+import { Login } from "../pages/Login";
 
-describe("App", () => {
-  it("renders Day 1 heading", () => {
-    render(<App />);
-    expect(
-      screen.getByText(/Day 1 – Auth Flow Base Setup/i)
-    ).toBeInTheDocument();
+describe("Login Component", () => {
+  test("should gogin successfuly", async () => {
+    //Arrange
+    render(
+      <MemoryRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+          </Routes>
+        </AuthProvider>
+      </MemoryRouter>
+    );
+    //Act
+    await userEvent.type(
+      screen.getByPlaceholderText("Email"),
+      "admin@test.com"
+    );
+    await userEvent.type(screen.getByPlaceholderText("Password"), "123456");
+    await userEvent.click(screen.getByRole("button", { name: /login/i }));
+
+    //Assert
+    expect(await screen.findByText(/dashboard/i)).toBeInTheDocument();
   });
 });
